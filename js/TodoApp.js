2eed58d5d0d6f33947a3247ee23213dcea0c4f59
Brian Ko <brian.kostar@gmail.com>
2014-12-20 19:59:19 -0500

    1.1

--- js/TodoApp.js
+++ js/TodoApp.js
@@ -1,7 +1,10 @@
+var taskNum = 1;
 function Item(task){
 	var self = this;
 	self.task = ko.observable(task);
-	self.time = ko.observable(new Date());
+	self.time = ko.observable(moment().format('h:mm:ss a'));
+	self.taskNum = taskNum;
+	taskNum++;
 }
 
 function TodoAppViewModel(){
@@ -13,7 +16,7 @@ function TodoAppViewModel(){
 
 	self.addTask = function(){
 		console.log('addTask called');
-		self.list.push(new Item("--"));
+		self.list.push(new Item(""));
 	};
 
 	self.deleteTask = function(task){
@@ -22,26 +25,22 @@ function TodoAppViewModel(){
 	};
 }
 
-// ko.applyBindings(new TodoAppViewModel());
-
 var Todo = new TodoAppViewModel();
 ko.applyBindings(Todo);
 
 var listExists = false;
 
 $('#createList').click(function(){
-	$( "#deleteList" ).toggle();
   	$( "#createList" ).toggle();
+  	$( ".todoList" ).removeClass('hide');
 	console.log('onclick create')
-	Todo.list.push(new Item("--"));
-	// var x = new Item('---')
-	// Todo.list = ko.observableArray([x]);
-	
+	Todo.list.push(new Item(""));	
 });
 
 $('#deleteList').click(function(){
+	$( ".todoList" ).addClass('hide');
 	$( "#createList" ).toggle();
-	$( "#deleteList" ).toggle();
 	console.log('onclick delete')
 	Todo.list.removeAll();
+	taskNum = 1;
 });
